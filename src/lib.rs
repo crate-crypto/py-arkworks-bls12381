9776c9cf08d9436aeead13c0e779e2f0c25df908
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-02-21 12:41:30 +0000

    pymodule needs to start with `py_arkworks_bls12381`

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,15 @@ mod wrapper;
 use pyo3::prelude::*;
 use wrapper::{G1Point, G2Point, Scalar, GT};
 
+// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
-fn word_counter(_py: Python, m: &PyModule) -> PyResult<()> {
+fn py_arkworks_bls12381(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<G1Point>()?;
     m.add_class::<G2Point>()?;
     m.add_class::<GT>()?;
